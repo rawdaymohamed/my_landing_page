35a364ad300be8526d78c25dd0e3f86e0cb20ef3
Rawda Yasser Mohamed <rawdaymohamed@gmail.com>
2025-02-24 06:23:26 +0200

    Fix: Add register button to the mobile nav

--- src/app/components/Home/Navbar/MobileNav.tsx
+++ src/app/components/Home/Navbar/MobileNav.tsx
@@ -1,14 +1,17 @@
 "use client";
+
 import { menuItems } from "@/app/lib/constants";
 import Link from "next/link";
 import React, { useState } from "react";
 import { IoMdClose } from "react-icons/io";
 import { RiMenu3Line } from "react-icons/ri";
+
 const MobileNav = () => {
   const [showMenu, setShowMenu] = useState(false);
 
   return (
-    <div>
+    <div className="relative flex items-center justify-end p-4 gap-5">
+      {/* Added flex container */}
       <div
         className={`${
           showMenu ? "translate-x-0" : "-translate-x-full"
@@ -17,7 +20,7 @@ const MobileNav = () => {
         <nav
           className={`${
             showMenu ? "translate-x-0 delay-200" : "-translate-x-full"
-          } transition-all duration-700 ease-in-out flex flex-col justify-center space-y-4 bg-indigo-900 h-full pl-14 w-[70%] sm:w-[70%] absolute top-0 left-0`} // Transition on inner nav with delay
+          } transition-all duration-700 ease-in-out flex flex-col justify-center space-y-4 bg-indigo-900 h-full pl-14 w-[70%] sm:w-[70%] absolute top-0 left-0`}
         >
           <IoMdClose
             className="cursor-pointer text-white size-6 absolute top-2 right-2"
@@ -34,9 +37,15 @@ const MobileNav = () => {
           ))}
         </nav>
       </div>
+      <Link
+        href="/"
+        className="px-6 py-1.5 bg-blue-600 hover:bg-blue-700 text-white rounded-full font-normal"
+      >
+        Join Now
+      </Link>
       <RiMenu3Line
-        className="absolute top-2 right-2 text-black size-6 cursor-pointer"
-        onClick={() => setShowMenu(!showMenu)} // Toggle menu open
+        className="text-black size-6 cursor-pointer" // Removed absolute and top/right here
+        onClick={() => setShowMenu(!showMenu)}
       />
     </div>
   );
